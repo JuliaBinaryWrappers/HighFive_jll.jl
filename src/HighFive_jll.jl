b2871bd1fb7c4c4a1c4b06d465a14ecf86a3a403
jlbuild <juliabuildbot@gmail.com>
2026-03-10 17:05:04 +0000

    HighFive_jll build 3.3.0+0

--- src/HighFive_jll.jl
+++ src/HighFive_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("HighFive")
-JLLWrappers.@generate_main_file("HighFive", UUID("714382d7-300d-5cff-9119-eab393adbf23"))
+JLLWrappers.@generate_main_file("HighFive", Base.UUID("714382d7-300d-5cff-9119-eab393adbf23"))
 end  # module HighFive_jll
